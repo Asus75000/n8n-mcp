b704f857502de6e38989bb5d97c3452c1f47ab2f
github-action-benchmark <github@users.noreply.github.com>
2025-09-06 17:00:38 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for fd6b3f95ad2ac1e5664ac9073676c0046d1daec7

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757178038447,
+  "repoUrl": "https://github.com/Asus75000/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "toz.fouhen@gmail.com",
+            "name": "Asus75000",
+            "username": "Asus93"
+          },
+          "committer": {
+            "email": "toz.fouhen@gmail.com",
+            "name": "Asus75000",
+            "username": "Asus93"
+          },
+          "distinct": true,
+          "id": "fd6b3f95ad2ac1e5664ac9073676c0046d1daec7",
+          "message": "Initial commit: n8n MCP server",
+          "timestamp": "2025-09-06T18:57:23+02:00",
+          "tree_id": "200043ef261b5926f4db3ee2bc157b1fe6848180",
+          "url": "https://github.com/Asus75000/n8n-mcp/commit/fd6b3f95ad2ac1e5664ac9073676c0046d1daec7"
+        },
+        "date": 1757178038158,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.019,
+            "range": "0.3537",
+            "unit": "ms",
+            "extra": "52629 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1797,
+            "range": "0.5719000000000003",
+            "unit": "ms",
+            "extra": "314 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "range": "0.24100000000000002",
+            "unit": "ms",
+            "extra": "211126 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0672,
+            "range": "0.3935",
+            "unit": "ms",
+            "extra": "14878 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
